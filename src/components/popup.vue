71bbc8018376f49a4a2e91ee3f82f4ae39d6bcc5
caoxiaoyi123 <285654767@qq.com>
2019-05-20 18:14:06 +0800

    正式包

--- src/components/popup.vue
+++ src/components/popup.vue
@@ -30,7 +30,7 @@
             </template>
             <template v-if="popupType=='closeTips'">
               <div class="tipBox">
-                <div class="txt-box">检测到您还未关注公众号，请先退出，关注公众号“标大大云数据”</div>
+                <div class="txt-box">检测到您还未关注公众号，请先退出，关注公众号“日精进定课”</div>
                 <!-- <button @click="cancelFn">确定</button> -->
               </div>
             </template>
